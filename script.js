fe40713d76f9497aaacf53fc7e17f2c8691a2aee
Myandes <2295126@cegepoutaouais.qc.ca>
2023-01-31 09:26:07 -0500

    progression3

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function Validation(){
-    let nom = document.forms[0]["nom"].value;
-    // Validation pour le nom
-if (nom.length <= 2 && nom.length >= 100)
-{        return true
-}    else
-{        alert("Le nom n'est pas valide")
-    return false    }
+form = document.getElementById("form");
+form.addEventListener('submit', function () { validatePhone() });
+
+function validatePhone() {
+    let province = document.getElementById("province").value;
+    let phone = document.getElementById("telephone").value;
+
+    if (province === "Ontario" && !phone.startsWith("613")) {
+        alert("Numéro de téléphone incorrect pour la province sélectionnée.");
+    } else if (province === "Quebec" && !phone.startsWith("819")) {
+        alert("Numéro de téléphone incorrect pour la province sélectionnée.");
+    } else {
+        alert("Numéro de téléphone valide pour la province sélectionnée.");
+    }
 }
